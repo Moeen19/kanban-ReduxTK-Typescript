8704eedc8501f59a6c89224132e4f7c6c76eb1b1
moeen <moeen12yt@gmail.com>
2024-07-15 20:43:06 +0500

    fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,7 +2,6 @@ import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 import "./globals.css";
 import Head from "next/head";
-import { Main, NextScript } from "next/document";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,19 +17,17 @@ interface GlobalLayoutProps {
 export default function RootLayout({ children }: GlobalLayoutProps) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <title>Todo Kanban</title>
-        <meta name="description" content="Todo project but with Typescript, Redux Toolkit and RTK Query." />
+        <meta property="og:description" content="Todo project but with Typescript, Redux Toolkit and RTK Query." />
         <meta property="og:title" content="Todo Kanban" />
         <meta property="og:description" content="Todo project but with Typescript, Redux Toolkit and RTK Query." />
         <meta property="og:image" content="https://kanban-redux-tk-typescript.vercel.app/ogImg.png" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://kanban-redux-tk-typescript.vercel.app/" />
-      </head>
+      </Head>
       <body className={`${inter.className} bg-[#2B187D] p-10`}>
         {children}
-       <Main />
-       <NextScript />
       </body>
     </html>
   );
