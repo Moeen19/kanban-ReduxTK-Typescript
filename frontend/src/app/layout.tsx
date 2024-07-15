4c863fc3f10e67bcda4eaf20dff0fd9ca5993370
moeen <moeen12yt@gmail.com>
2024-07-15 21:33:51 +0500

    kek

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -39,11 +39,6 @@ export default function RootLayout({ children }: GlobalLayoutProps) {
         <meta property="og:description" content={metadata.openGraph.description} />
         <meta property="og:image" content={metadata.openGraph.images[0].url} />
         <meta property="og:image:secure_url" content={metadata.openGraph.images[0].url} />
-        {/* <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
-        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
-        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:type" content={metadata.openGraph.type} /> */}
       </Head>
       <body className={`${inter.className} bg-[#2B187D] p-10`}>
         {children}
