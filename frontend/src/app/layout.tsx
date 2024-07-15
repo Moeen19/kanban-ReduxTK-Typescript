ccea8092fa1d7e12941939fe36b26210cdc7f4fd
moeen <moeen12yt@gmail.com>
2024-07-15 20:32:16 +0500

    kekw

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,20 +9,20 @@ interface GlobalLayoutProps {
   children: ReactNode
 }
 
-export const metadata = {
-  title: "Todo Kanban",
-  description: "Todo Kanban but with typescript, redux toolkit and rtk query.",
-};
+// export const metadata = {
+//   title: "Todo Kanban",
+//   description: "Todo Kanban but with typescript, redux toolkit and rtk query.",
+// };
 
 export default function RootLayout({ children }: GlobalLayoutProps) {
   return (
     <html lang="en">
-      {/* <head>
+      <head>
         <title>Todo Kanban</title>
         <meta property="og:title" content="Todo Kanban" />
-        <meta property="og:description" content="MERN-Todo project but with Typescript, Redux Toolkit and RTK Query." />
+        <meta property="og:description" content="Todo project but with Typescript, Redux Toolkit and RTK Query." />
         <meta property="og:image" content="/ogImg.png" />
-      </head> */}
+      </head>
       <body className={`${inter.className} bg-[#2B187D] p-10`}>
         {children}
       </body>
