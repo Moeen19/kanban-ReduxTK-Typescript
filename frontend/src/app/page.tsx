b6ebda8603141da9374d4fe4e1942d8c32a15a7e
moeen <moeen12yt@gmail.com>
2024-07-16 11:20:54 +0500

    width

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -48,7 +48,7 @@ const HomeContent = () => {
 
   return (
     <main className=''>
-      {loader && <div className="flex gap-[5px] inset-0 m-auto">
+      {loader && <div className="flex gap-[5px] max-w-fit w-full inset-0 m-auto">
         <div className='h-4 w-4 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
         <div className='h-4 w-4 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
         <div className='h-4 w-4 bg-white rounded-full animate-bounce'></div>
