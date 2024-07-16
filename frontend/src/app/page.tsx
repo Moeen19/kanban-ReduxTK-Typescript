bd1452339096108a8f1157616bea0f3bebf63924
moeen <moeen12yt@gmail.com>
2024-07-16 11:14:56 +0500

    animated loader

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -48,7 +48,7 @@ const HomeContent = () => {
 
   return (
     <main className=''>
-      {loader && <div className="flex">
+      {loader && <div className="flex gap-[10px] inset-0 m-auto">
         <div className='h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
         <div className='h-4 w-4 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
         <div className='h-4 w-4 bg-black rounded-full animate-bounce'></div>
